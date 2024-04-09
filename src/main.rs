8ff790c8de19038653022f6f0165b7db71ecf883
Wei-ying Chen <weiying.alex.chen@gmail.com>
2024-04-09 21:17:30 +0800

    Restore http request

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,11 @@ fn main() -> anyhow::Result<()> {
 
     let peripherals = Peripherals::take()?;
 
-    // init scale
-
     let dt = peripherals.pins.gpio2;
     let sck = peripherals.pins.gpio3;
     let mut scale = Scale::new(sck, dt, LOAD_SENSOR_SCALING).unwrap();
 
     scale.tare(32);
-
-    // init wifi
 
     let mut wifi = Wifi::new(peripherals.modem)?;
 
@@ -42,36 +38,31 @@ fn main() -> anyhow::Result<()> {
 
         log::info!("Wifi: success!");
 
-        // // initi http
+        let headers = [
+            ("apikey", SUPABASE_KEY),
+            ("Authorization", &format!("Bearer {}", SUPABASE_KEY)),
+            ("Content-Type", "application/json"),
+            ("Prefer", "return=representation"),
+            // ("Content-Length", &content_length_header),
+        ];
 
-        // let headers = [
-        //     ("apikey", SUPABASE_KEY),
-        //     ("Authorization", &format!("Bearer {}", SUPABASE_KEY)),
-        //     ("Content-Type", "application/json"),
-        //     ("Prefer", "return=representation"),
-        //     // ("Content-Length", &content_length_header),
-        // ];
+        let mut http = Http::new(&SUPABASE_URL, &headers)?;
 
-        // let mut http = Http::new(&SUPABASE_URL, &headers)?;
-        // // let mut iterations = 0;
+        let payload_bytes = get_readings(&mut scale)?;
 
-        // let payload_bytes = collect_readings(&mut scale)?;
-
-        // http.post(&payload_bytes)?;
+        http.post(&payload_bytes)?;
 
         info!("Shutting down in 5s...");
 
         wifi.disconnect()?;
 
-        // info!("Wifi disconnected!");
+        info!("Wifi disconnected!");
 
         FreeRtos::delay_ms(10000u32);
     }
-
-    Ok(())
 }
 
-fn collect_readings(scale: &mut Scale<'_, Gpio3, Gpio2>) -> anyhow::Result<Vec<u8>> {
+fn get_readings(scale: &mut Scale<'_, Gpio3, Gpio2>) -> anyhow::Result<Vec<u8>> {
     let mut readings = Vec::new();
 
     loop {
@@ -80,23 +71,19 @@ fn collect_readings(scale: &mut Scale<'_, Gpio3, Gpio2>) -> anyhow::Result<Vec<u
         if scale.is_ready() {
             log::info!("Scale: success!");
 
-            // Read scale and create message
             let rounded_reading = scale.read_rounded().unwrap();
             let message = format!("Weight: {} g", rounded_reading);
             log::info!("{}", message);
 
-            // Serialize message to JSON
             let payload = serde_json::json!({ "content": message });
             let payload_str = serde_json::to_string(&payload)?;
             let mut payload_bytes = payload_str.into_bytes();
 
-            // Add a newline as a delimiter for each reading
             payload_bytes.push(b'\n');
 
-            // Concatenate this reading's bytes to the overall readings vector
             readings.extend(payload_bytes);
 
-            break; // Remove this if you want to collect multiple readings
+            break;
         }
     }
 
